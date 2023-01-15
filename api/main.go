0b3c4c0c61c53f89e7c3e710f3bf6ee00137e1e5
nakashi94 <n0904yBUNJOU@gmail.com>
2023-01-15 23:53:45 +0900

    run server and test api router

--- api/main.go
+++ api/main.go
@@ -1,7 +1,49 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+type Recipe struct {
+	ID   string `json:"id"`
+	Name string `json:"name"`
+}
+
+var recipes = []Recipe{
+	{ID: "1", Name: "a"},
+	{ID: "2", Name: "b"},
+}
+
+type Album struct {
+	ID     string  `json:"id"`
+	Title  string  `json:"title"`
+	Artist string  `json:"artist"`
+	Price  float64 `json:"price"`
+}
+
+var albums = []Album{
+	{ID: "1", Title: "Blue Train", Artist: "John Coltrane", Price: 56.99},
+	{ID: "2", Title: "Jeru", Artist: "Gerry Mulligan", Price: 17.99},
+	{ID: "3", Title: "Sarah Vaughan and Clifford Brown", Artist: "Sarah Vaughan", Price: 39.99},
+}
 
 func main() {
 	fmt.Println("Hello GO!")
+
+	router := gin.Default()
+	router.GET("/v1/api/recipes", getAllRecipes)
+	router.GET("/albums", getAlbums)
+
+	router.Run("localhost:8080")
+}
+
+func getAllRecipes(ctx *gin.Context) {
+	ctx.IndentedJSON(http.StatusOK, recipes)
+}
+
+func getAlbums(ctx *gin.Context) {
+	ctx.IndentedJSON(http.StatusOK, albums)
 }
