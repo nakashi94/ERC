01d453e717c8f51b5b57024f97978bc8f55101d7
nakashi94 <n0904yBUNJOU@gmail.com>
2023-01-17 23:22:47 +0900

    fix: delete albums

--- api/main.go
+++ api/main.go
@@ -18,19 +18,6 @@ var recipes = []Recipe{
 	{ID: "2", Name: "b"},
 }
 
-type Album struct {
-	ID     string  `json:"id"`
-	Title  string  `json:"title"`
-	Artist string  `json:"artist"`
-	Price  float64 `json:"price"`
-}
-
-var albums = []Album{
-	{ID: "1", Title: "Blue Train", Artist: "John Coltrane", Price: 56.99},
-	{ID: "2", Title: "Jeru", Artist: "Gerry Mulligan", Price: 17.99},
-	{ID: "3", Title: "Sarah Vaughan and Clifford Brown", Artist: "Sarah Vaughan", Price: 39.99},
-}
-
 func main() {
 	fmt.Println("Hello GO!")
 
@@ -40,15 +27,11 @@ func main() {
 	// router.Engine.Group("/v1/api")
 	// router.Group("/v1/api")
 	// router.GET("/recipes", getAllRecipes)
-	router.Engine.GET("/albums", getAlbums)
+	router.Engine.GET("/v1/api/recipes", getAllRecipes)
 
 	router.Engine.Run("localhost:8080")
 }
 
 func getAllRecipes(ctx *gin.Context) {
 	ctx.IndentedJSON(http.StatusOK, recipes)
-}
-
-func getAlbums(ctx *gin.Context) {
-	ctx.IndentedJSON(http.StatusOK, albums)
 }
