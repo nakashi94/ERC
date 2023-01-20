8f10e1d283c65c2003abd6bd809456bc88aa02aa
nakashi94 <n0904yBUNJOU@gmail.com>
2023-01-21 00:02:24 +0900

    getAllRecipes

--- api/main.go
+++ api/main.go
@@ -1,18 +1,8 @@
 package main
 
 import (
-	"net/http"
-	"web-service-api/models"
 	"web-service-api/routes"
-
-	"github.com/gin-gonic/gin"
 )
-
-var recipes = []models.Recipe{
-	{ID: "fd", UserID: "abc", DishName: "a", URL: "http", Category: "curry", Media: "YouTube", Repeat: "1", CookingTime: 20, CreatedAt: "2023-01-18 12:30:24.241566", UpdatedAt: "2023-01-18 12:30:24.241566"},
-	{ID: "fd", UserID: "abc", DishName: "a", URL: "http", Category: "curry", Media: "YouTube", Repeat: "1", CookingTime: 20, CreatedAt: "2023-01-18 12:30:24.241566", UpdatedAt: "2023-01-18 12:30:24.241566"},
-	{ID: "fd", UserID: "abc", DishName: "a", URL: "http", Category: "curry", Media: "YouTube", Repeat: "1", CookingTime: 20, CreatedAt: "2023-01-18 12:30:24.241566", UpdatedAt: "2023-01-18 12:30:24.241566"},
-}
 
 func main() {
 	router := routes.NewRouter()
@@ -20,12 +10,7 @@ func main() {
 	router.SetMiddleware()
 	router.SetProxy()
 	router.SetHealthChecker()
-	router.Engine.GET("/v1/api/recipes", getAllRecipes)
+	router.NewTaskRouter()
 
 	router.Serve()
-}
-
-// 全てのレシピデータを取得する
-func getAllRecipes(ctx *gin.Context) {
-	ctx.IndentedJSON(http.StatusOK, recipes)
 }
